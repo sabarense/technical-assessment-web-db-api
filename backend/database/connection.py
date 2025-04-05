b88296aa763fb44be61f0f54661c4c65d007ba42
Yan Sabarense <sabarensey@gmail.com>
2025-04-05 15:18:48 -0300

    [feat] db connect

--- backend/database/connection.py
+++ backend/database/connection.py
@@ -1,6 +1,8 @@
 import sqlite3
+from backend.database.schema import criar_tabelas
 
 def create_connection():
     conn = sqlite3.connect(':memory:')
     conn.execute('PRAGMA foreign_keys = ON;')
+    criar_tabelas(conn)
     return conn
